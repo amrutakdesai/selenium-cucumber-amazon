e18fa298ab72a2dcf4695805668c00607d54df37
amrutakdesai <amrutakdesai1008@gmail.com>
2022-07-01 16:53:04 +0530

    modified the screenshot method or dependency

--- src/test/java/com/skorek/PageObjects/ProductCategoryPage.java
+++ src/test/java/com/skorek/PageObjects/ProductCategoryPage.java
@@ -10,7 +10,7 @@ public class ProductCategoryPage extends PageObject {
     }
 
     public ProductPage goToProductOnPosition(int position){
-        String xpath_to_position = String.format("//*[@id=\"zg_centerListWrapper\"]/div[%s]/div[2]/div/a/div[1]/img", position+1);
+        String xpath_to_position = String.format("//*[@id='zg_centerListWrapper']/div[%s]/div[2]/div/a/div[1]/img", position+1);
         WebElement element =  driver.findElement(By.xpath(xpath_to_position));
         element.click();
         return new ProductPage(driver);

897a2ee92f93089769dfb66d8eb6dde1c8163645
amrutakdesai <97360236+amrutakdesai@users.noreply.github.com>
2022-07-14 10:52:23 +0530

    new

--- src/test/java/com/skorek/PageObjects/newClass.java
+++ src/test/java/com/skorek/PageObjects/newClass.java
@@ -3,7 +3,7 @@ package com.skorek.PageObjects;
 public class newClass {
     public void up()
     {
-        System.out.println("Changes in my test branch");
+        System.out.println("");
     }
 
 }
